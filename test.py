65033e9ba4cbf5d06b7b710dad123414e735ab8e
starcraftchobo <madeinburundi@gmail.com>
2024-08-29 17:55:53 +0900

    robot

--- test.py
+++ test.py
@@ -1,35 +1,17 @@
+# a = [11, 12, 0, 24, 0, 0, 0, 0, 0, 0, 0, 20, 0, 21, 0, 19, 0, 0, 14, 9, 0, 15, 0, 23, 0]
+# b=0
+# for j in range(5):
+#     print(a[5*j])
+#     print(a[5*j + 1])
+#     print(a[5*j + 2])
+#     print(a[5*j + 3])
+#     print(a[5*j + 4])
+#     if a[5*j] and a[5*j + 1] and a[5*j + 2] and a[5*j + 3] and a[5*j + 4] != True:
+#         b += 1
+#
+# print(b)
 
-'''
-13
-1 2 1 3 2 4 3 5 3 6 4 7 5 8 5 9 6 10 6 11 7 12 11 13
-'''
-
-def in_order(T):
-    result = ''
-    if T:
-        result += in_order(left[T])     # 왼쪽 먼저 탐색
-        result += word[T]               # 더 이상 왼쪽에 값이 없으면 해당 노드의 단어를 result에 넣고 오른쪽 보기
-        result += in_order(right[T])    # 오른쪽 노드로 이동해서 없으면 0이면 그냥 반환 -> 지금까지 result 가지고 부모 노드로, 있으면 해당 노드의 왼쪽부터 다시 보기
-
-    return result
-
-T = 10
-
-for tc in range(1, T+1):
-    N = int(input())
-    arr = [list(map(lambda x:int(x) if x.isdecimal() else x, input().split())) for _ in range(N)]      # 노드번호, 노드문자, 연결된 노드
-
-    left = [0] * (N+1)      # idx 0 고려해서 생성
-    right = [0] * (N+1)
-    word = [0] * (N+1)      # 각 노드의 단어를 기록할 리스트
-
-    for x in arr:
-        word[x[0]] = x[1]   # 리스트에서 단어는 idx 맞춰서 word에 기록
-        if len(x) == 4:     # 리스트에 left, right 정보가 다 있으면 둘 다 기록
-            left[x[0]] = x[2]
-            right[x[0]] = x[3]
-
-        elif len(x) == 3:   # 리스트에 left 정보만 있으면 left만 기록
-            left[x[0]] = x[2]
-
-    print(f'#{tc} {in_order(1)}')       # root는 1
+a, b, c, d, e = 0, 0, 0, 0, 0
+print(a, b, c, d, e)
+if a or b or c or d or e:
+    print('5')
